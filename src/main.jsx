df64fa089169277b0ebbb927fb541690d1206a03
oliadesigner <olaledovskih860@gmail.com>
2026-04-01 17:21:40 +0300

    fix: posthog env-based host config

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import posthog from 'posthog-js';
 
-posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
-  api_host: '/ingest',
-  ui_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-  person_profiles: 'identified_only',
+posthog.init('phc_qDpWqykcdpSVSAcffGDvwUrDGZA6FiVqG5HujnSWGpRW', {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+  ui_host: 'https://us.posthog.com',
+  person_profiles: 'always',
   session_recording: {
     maskAllInputs: true,
   },
-});
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
