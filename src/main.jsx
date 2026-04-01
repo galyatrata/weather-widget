1638300202343a2aed4724aefbaaa709e3fe50e2
oliadesigner <olaledovskih860@gmail.com>
2026-04-01 16:26:41 +0300

    fix: add vercel csp headers

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import posthog from 'posthog-js';
 
-posthog.init('phc_qDpWqykcdpSVSAcffGDvwUrDGZA6FiVqG5HujnSWGpRW', {
-  api_host: 'https://us.posthog.com',
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
   person_profiles: 'identified_only',
   session_recording: {
     maskAllInputs: true,
