0ccd9cbd0eb7d39080c5d9008b0129fab00a1428
oliadesigner <olaledovskih860@gmail.com>
2026-04-05 23:04:43 +0300

    fix: add posthog init

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import posthog from 'posthog-js';
+import posthog from 'posthog-js'
 
 posthog.init('phc_qDpWqykcdpSVSAcffGDvwUrDGZA6FiVqG5HujnSWGpRW', {
   api_host: 'https://us.i.posthog.com',
